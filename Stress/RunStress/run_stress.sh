baa3eb3777508d80cfdb3f0ca290fbcadf155d90
threck <threck@qq.com>
2020-12-31 17:07:36 +0800

    fix mongodb config

--- Stress/RunStress/run_stress.sh
+++ Stress/RunStress/run_stress.sh
@@ -2,4 +2,5 @@ bash start_server.sh
 sleep 1s
 bash start_client_multi.sh 4
 sleep 1s
-echo "logs:$(ls -l tmp|awk -F' ' '{print $9}')"
+echo "logs dir: tmp/"
+echo "logs file: $(ls -l tmp|awk -F' ' '{print $9}')"
